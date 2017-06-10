4e99f10c88bac1d89036a51583595c6012d405c7
Luke Ryan <luke.ryan@xero.com>
2017-06-10 17:39:46 +1200

    Add script to fix gestures on macbook as they crash weekly

---
+++ fix-osx-gestures.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Fixes the touchpad gestures on my 2017 13' Macbook Pro when they stop working.
+# Ref: https://apple.stackexchange.com/questions/151907/how-to-bring-back-multi-touch-gestures-after-it-crashes-without-reboot
+killall Dock
